f684ffa7ad712fea99d2f58fbf7ce3af268c97ef
Aanika Mishra <a-mishra@onu.edu>
2024-11-21 16:00:11 -0500

    GOT IT

--- Laboratory 11.cpp
+++ Laboratory 11.cpp
@@ -11,80 +11,72 @@
 
 using namespace std;
 
-//const int ROWS = 13;
+const int ROWS = 13;
 
-//int getRow(char value[3]);
 
-/*
 int findRowIndex(int thisRow);
 int findSeatIndex(char thisSeat);
 void displaySeatColumn(char seats[][ROWS], int column);
-*/
+
 
 int main()
 {
-    char seats[13][4] = { {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'},
-                          {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'},
-                          {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', 'C', 'D'}, {'A', 'B', ' ', ' '}};
-    char rows[13][2] = { {' ', '1'}, {' ', '2'}, {' ', '3'}, {' ', '4'}, {' ', '5'}, {' ', '6'}, {' ', '7'}, {' ', '8'}, {' ', '9'}, {'1', '0'}, { '1', '1' }, {'1', '2'}, {'1', '3'} };
-    string seat;
+    char seats[8][ROWS] = 
+    {
+    {'D','D','D','D','D','D','D','D','D','D','D','D',' '},
+    {'C','C','C','C','C','C','C','C','C','C','C','C',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+    {' ',' ',' ',' ',' ',' ',' ',' ',' ','1','1','1','1'},
+    {'1','2','3','4','5','6','7','8','9','0','1','2','3'},
+    {' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+    {'B','B','B','B','B','B','B','B','B','B','B','B','B'},
+    {'A','A','A','A','A','A','A','A','A','A','A','A','A'}
+    };
+    int row;
+    char seat;
     bool done = false;
 
-    //DISPLAYS THE SEATING CHART
-    for (int i = 4; i > 0; i--)
-    {
-        for (int j = 0; j < 13; j++)
-        {
-            cout << seats[j][i-1] << ' ';
-        }
-        cout << "\n";
-        if (i == 3)
-        {
-            cout << "\n";
-            for (int i = 0; i < 13; i++)
-            {
-                cout << rows[i][0] << ' ';
-            }
-            cout << "\n";
-            for (int i = 0; i < 13; i++)
-            {
-                cout << rows[i][1] << ' ';
-            }
-            cout << "\n" << endl;
-        }   
-    }
-    cout << "\n";
-    //END SEATING CHART
+    
 
     //Loop for Q
     while (!done)
     {
-        cout << "Enter a seat or Q to quit: ";
-        getline(cin, seat);
+        for (int i = 0; i < 8; i++)
+        {
+            displaySeatColumn(seats, i);
+        }
 
-        if (seat == "Q")
+        cout << "\nEnter a seat or Q to quit: ";
+        cin >> row >> seat;
+
+        if (cin.fail())
         {
             done = true;
             break;
-        }        
+        }      
+
+        if (row < 1 || row > 13)
+        {
+            cout << "Sorry, this seat does not exist on the CRJ 200." << endl;
+        }
+        else if (findSeatIndex(seat) == -1)
+        {
+            cout << "Sorry, this seat does not exist on the CRJ 200." << endl;
+        }
+        else if (row == 13 && (findSeatIndex(seat) == 0 || findSeatIndex(seat) == 1))
+        {
+            cout << "Sorry, this seat does not exist on the CRJ 200." << endl;
+        }
+        else
+        {
+            seats[findSeatIndex(seat)][findRowIndex(row)] = 'X';
+        }
+        cout << "\n";
     }
     
 
 
 
-}
-/*
-int getRow(char value[3])
-{
-    int digit = 0;
-    int num = value[0];
-    switch (num)
-    {
-      case 49: digit = 1;
-      case 50: digit = 2;
-      case 51: digit = 3;
-    }
-    return digit;
 }
 
 // findRowIndex: given user input, returns appropriate row index into arrays.
@@ -92,25 +84,33 @@ int getRow(char value[3])
 // @return appropriate index into 2-D array if the specified row is valid, ERROR otherwise
 int findRowIndex(int thisRow)
 {
-    
+    return thisRow - 1;
 }
 // findSeatIndex: given user input, returns appropriate column index into seating chart array.
 // @param thisSeat - character indicating selected seat within row
 // @return appropriate index into 2-D array if the specified seat is valid, ERROR otherwise
 int findSeatIndex(char thisSeat)
 {
-
+    switch (thisSeat)
+    {
+    case 'D': return 0;
+    case 'C': return 1;
+    case 'B': return 6;
+    case 'A': return 7;
+    }
+    return -1;
 }
+
 // displaySeatColumn: given a 2-D mapping of the seating chart in the form [columns][rows],
 // will print out a "column" of seats on one row of output (i.e., seat 'A' for all rows in the plane)
 // @param seats - reference to a 2-D character array
 // @param column - reference to which column of seats to display
 void displaySeatColumn(char seats[][ROWS], int column)
 {
-    for(int i = 0; i < ROWS; i++)
-    {
-
-    }
+        for (int i = 0; i < ROWS; i++) 
+        {
+		    cout << seats[column][i] << " ";
+	    }
+	    cout << "\n";
 
 }
-*/
